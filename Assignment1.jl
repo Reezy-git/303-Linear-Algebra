b300c24d94ca503729917eeadc0abe6af3e0d8c5
Reezy-git <36226936+Reezy-git@users.noreply.github.com>
2022-08-24 22:32:32 +1200

    Update Assignment1.jl

--- Assignment1.jl
+++ Assignment1.jl
@@ -102,10 +102,74 @@ $\vec{v}=M-θ*e₂=\left[\begin{array}{c}
 \end{array}\right]$
 """
 
+# ╔═╡ ace59a76-703d-49eb-bea3-479c245ac022
+H
+
+# ╔═╡ fe69bf18-119a-48ea-bee5-1ba5faa0b678
+md"""
+The outterproduct of our vector *v* gives us the householder matrix H
+
+$H=\frac{1}{3}\left[\begin{array}{c}
+1 & 0 & 0 & 0\\
+0 & -2 & 1 & 2
+\end{array}\right]$
+"""
+
 # ╔═╡ 90ef3c8a-c8d7-47e6-b7e6-0e10a72a255f
 md"""
 Givens Rotations:
 """
+
+# ╔═╡ e0121f81-04db-4c35-af63-867fc073cb16
+begin
+    function givens_rotation(a, b)
+        if b == 0
+            c = sign(a)
+            if c == 0
+                c = 1
+            end
+        elseif a == 0
+            c = 0
+            s = sign(b)
+            r = abs(b)
+        elseif abs(a) > abs(b)
+            t = b/a
+            u = sign(a) * √(1 + t^2)
+            c = 1 / u
+            s = c * t
+            r = a * u
+        else
+            t = a / b
+            u = sign(b) * √(1 + t^2)
+            s = 1 / u
+            c = s * t
+            r = b * u
+        end
+        return c, s, r
+    end
+
+    G₁=Matrix{Float64}(I, 4, 4)
+
+    A = [
+        3 4
+        0 -2
+        0 1
+        0 2
+    ]
+
+    i, j = 3, 2
+
+    r, c, s = givens_rotation(A[i-1,j], -A[i,j])
+
+    G₁[i,j] = s / r
+    G₁[j,j] = c/ r
+    G₁[i,i] = c/ r
+    G₁[j,i] = -s/ r
+
+    G₁
+
+    G₁*A
+end;
 
 # ╔═╡ 825e4aca-45d4-42af-8c50-1ab5ee670bad
 md"""
@@ -141,6 +205,9 @@ $Aᵀ=\frac{1}{3}\left[\begin{array}{cc}
 1 & 4\\
 2 & 5\end{array}\right]$
 """
+
+# ╔═╡ b4469009-a045-4932-bc26-0b8943d8ff83
+
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -185,13 +252,17 @@ uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
 # ╟─910ed377-7895-4da6-ad1d-d2fa5a78d8ca
 # ╟─a01b5da6-9d2d-429b-ac55-93b8a3858a99
 # ╟─3930a39b-c26b-4a42-a3da-8550e2fce0f9
-# ╟─9cdf0092-5d3e-4521-8d4a-07c962a0772d
+# ╠═9cdf0092-5d3e-4521-8d4a-07c962a0772d
 # ╟─64a5e043-7b71-4559-86cc-a316bfc3d668
 # ╟─882a0b1c-85d8-475f-be5e-1828faa10196
 # ╟─7a949596-5c6e-4faa-8971-154774836859
+# ╟─ace59a76-703d-49eb-bea3-479c245ac022
+# ╟─fe69bf18-119a-48ea-bee5-1ba5faa0b678
 # ╟─90ef3c8a-c8d7-47e6-b7e6-0e10a72a255f
+# ╟─e0121f81-04db-4c35-af63-867fc073cb16
 # ╟─825e4aca-45d4-42af-8c50-1ab5ee670bad
 # ╟─41fc5b67-869c-4b1c-bb5e-cbcd6d2217c1
 # ╟─470ae356-7ebe-4d9a-b582-b3796e36d7c1
+# ╠═b4469009-a045-4932-bc26-0b8943d8ff83
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
